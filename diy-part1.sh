21bc8ec3e32e1f9c1dfc7d931660af0732e01c80
xiaoyuanpang <46011876+xiaoyuanpang@users.noreply.github.com>
2026-05-13 21:13:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,9 +10,7 @@
 # See /LICENSE for more information.
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i "/helloworld/d" "feeds.conf.default"
 
 # Add a feed source
+sed -i "/helloworld/d" "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
